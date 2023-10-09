97f4257f8ede62d089ddee7af3b3651542b60572
Nikolay Kovalenko <nikolay.kovalenko@humanitec.com>
2023-10-09 21:56:44 +0200

    update

--- iam-policy-s3-public/main.tf
+++ iam-policy-s3-public/main.tf
@@ -9,6 +9,11 @@ data "aws_iam_policy_document" "public" {
 
     effect = "Allow"
 
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
     actions = [
       "s3:GetObject",
       "s3:ListBucket",
